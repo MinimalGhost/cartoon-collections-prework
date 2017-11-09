76255087abc58b1fa6685a86d8623b8a5c1bcc94
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 19:49:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,11 @@ end
 
 def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  matches = foods & cheese_types
-  matches.each do |cheese|
-    return "#{cheese}"
+  if matches = foods & cheese_types
+    matches.each do |cheese|
+      return "#{cheese}"
+    end
+  else
+
   end
 end
